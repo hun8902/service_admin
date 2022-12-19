ad50b6a64d1188fe15a8888d31f2fa87cf73bac2
hun8902 <hun8902@bilrigo.com>
2022-12-19 15:28:59 +0900

    DB 정리 설명 추가

--- config/db_con.php
+++ config/db_con.php
@@ -1,11 +1,7 @@
 <?php
-session_start();
-
-
-
 class srms
 {
-    public $base_url = '';
+    public $base_url = 'http://service.bilrigo.net/';
     public $connect;
     public $query;
     public $statement;
@@ -13,7 +9,7 @@ class srms
 
     function srms()
     {
-        $this->connect = new PDO("mysql:host=localhost;dbname=k890210", "", "");
+        $this->connect = new PDO("mysql:host=localhost;dbname=admin", "root", "t6EuZwCHXH6tQr2yKNWCM4b9v3FGt6d3");
         session_start();
         $this->now = date("Y-m-d H:i:s",  STRTOTIME(date('h:i:sa')));
     }
@@ -34,6 +30,11 @@ class srms
     function row_count()
     {
         return $this->statement->rowCount();
+    }
+    
+    function lastInsertIdsave()
+    {
+        return $this->connect->lastInsertId();
     }
 
     function statement_result()
@@ -150,7 +151,7 @@ class srms
             return $row["cp_name"];
         }
     }
-
+    
 
     function clean_input($string)
     {
@@ -290,136 +291,6 @@ class srms
         {
             return $row["Total"];
         }
-    }
-
-}
-
-$db = new db(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE);
-
-
-class db
-{
-
-    protected $connection;
-    protected $query;
-    public $query_count = 0;
-
-    public function __construct($dbhost = 'localhost', $dbuser = 'root', $dbpass = '', $dbname = '', $charset = 'utf8')
-    {
-        $this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-        if ($this->connection->connect_error) {
-            die('Failed to connect to MySQL - ' . $this->connection->connect_error);
-        }
-        $this->connection->set_charset($charset);
-    }
-
-    public function query($query)
-    {
-        if ($this->query = $this->connection->prepare($query)) {
-            if (func_num_args() > 1) {
-                $x = func_get_args();
-                $args = array_slice($x, 1);
-                $types = '';
-                $args_ref = array();
-                foreach ($args as $k => &$arg) {
-                    if (is_array($args[$k])) {
-                        foreach ($args[$k] as $j => &$a) {
-                            $types .= $this->_gettype($args[$k][$j]);
-                            $args_ref[] = &$a;
-                        }
-                    } else {
-                        $types .= $this->_gettype($args[$k]);
-                        $args_ref[] = &$arg;
-                    }
-                }
-                array_unshift($args_ref, $types);
-                call_user_func_array(array($this->query, 'bind_param'), $args_ref);
-            }
-            $this->query->execute();
-            if ($this->query->errno) {
-                die('Unable to process MySQL query (check your params) - ' . $this->query->error);
-            }
-            $this->query_count++;
-        } else {
-            die('Unable to prepare statement (check your syntax) - ' . $this->connection->error);
-        }
-        return $this;
-    }
-
-    public function fetchAll()
-    {
-        $params = array();
-        $meta = $this->query->result_metadata();
-        while ($field = $meta->fetch_field()) {
-            $params[] = &$row[$field->name];
-        }
-        call_user_func_array(array($this->query, 'bind_result'), $params);
-        $result = array();
-        while ($this->query->fetch()) {
-            $r = array();
-            foreach ($row as $key => $val) {
-                $r[$key] = $val;
-            }
-            $result[] = $r;
-        }
-        $this->query->close();
-        return $result;
-    }
-
-    public function fetchArray()
-    {
-        $params = array();
-        $meta = $this->query->result_metadata();
-        while ($field = $meta->fetch_field()) {
-            $params[] = &$row[$field->name];
-        }
-        call_user_func_array(array($this->query, 'bind_result'), $params);
-        $result = array();
-        while ($this->query->fetch()) {
-            foreach ($row as $key => $val) {
-                $result[$key] = $val;
-            }
-        }
-        $this->query->close();
-        return $result;
-    }
-
-    public function numRows()
-    {
-        $this->query->store_result();
-        return $this->query->num_rows;
-    }
-
-    public function insertedId()
-    {
-        return $this->query->insert_id;
-    }
-
-    public function close()
-    {
-        return $this->connection->close();
-    }
-
-    public function affectedRows()
-    {
-        return $this->query->affected_rows;
-    }
-
-    private function _gettype($var)
-    {
-        if (is_string($var)) {
-            return 's';
-        }
-
-        if (is_float($var)) {
-            return 'd';
-        }
-
-        if (is_int($var)) {
-            return 'i';
-        }
-
-        return 'b';
     }
 
 }
